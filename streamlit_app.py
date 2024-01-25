d373cc8731f70e165449a9dde80e87dd1e712903
Benjamin Mellon <97412534+Cantaloupe2@users.noreply.github.com>
2024-01-24 22:54:06 -0500

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,24 @@
+st.write("Chem Molecule Calculator")
+
+
+
+    st.markdown(
+        """
+        To calculate molecular structure simply enter the molecular weight. 
+
+        It will spit out all possible comginations of (H,C,O)
+        """
+
+    )
+
+    input = st.text_input("Enter Molecular Formula Here As an Integer")
+    weight = int(input)
+    i_max = weight
+    j_max = weight/12
+    k_max = weight/16
+    for i in range(i_max):
+        for j in range(j_max):
+            for k in range(k_max):
+              sum = i*1+j*12+k*16
+              if sum == weight:
+                  print(f"({i},{j},{k})")
