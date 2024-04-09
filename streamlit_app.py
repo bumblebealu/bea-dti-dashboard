9c92866daa50fe8b8e6ba2870dcbfa5dc8e3eba0
bumblebealu <lugirl131415@gmail.com>
2024-04-08 22:00:57 -0400

    layers edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,9 +19,9 @@ def find_pairs(countries, target_country):
     leaving_pairs = []
     for i in range(1, len(countries)):
         if countries[i] == target_country and countries[i-1] != target_country:
-            entering_pairs.append((countries[i-1], countries[i]))
+            entering_pairs.append(countries[i-1])
         elif countries[i-1] == target_country and countries[i] != target_country:
-            leaving_pairs.append((countries[i-1], countries[i]))
+            leaving_pairs.append(countries[i])
     return entering_pairs, leaving_pairs
 
 def process_data(df, target_country):
@@ -32,27 +32,59 @@ def process_data(df, target_country):
         entering.extend(e)
         leaving.extend(l)
     
-    entering_df = pd.DataFrame(entering, columns=['from_country', 'to_country'])
-    leaving_df = pd.DataFrame(leaving, columns=['from_country', 'to_country'])
+    entering_freq = pd.Series(entering).value_counts().reset_index(name='entering_frequency')
+    leaving_freq = pd.Series(leaving).value_counts().reset_index(name='leaving_frequency')
 
-    entering_freq = entering_df['from_country'].value_counts().reset_index(name='frequency')
-    leaving_freq = leaving_df['to_country'].value_counts().reset_index(name='frequency')
-    
-    return entering_freq, leaving_freq
+    merged_df = pd.merge(
+        entering_freq.rename(columns={'index': 'country'}),
+        leaving_freq.rename(columns={'index': 'country'}),
+        on='country',
+        how='outer'
+    ).fillna(0)
 
-def plot_choropleth(geojson_data, data, title, fill_color):
+    merged_df['net_frequency'] = merged_df['entering_frequency'] - merged_df['leaving_frequency']
+
+    return merged_df
+
+def plot_choropleth(geojson_data, data, target_country):
     m = folium.Map(location=[20, 0], zoom_start=2)
+
     folium.Choropleth(
         geo_data=geojson_data,
-        name=title,
+        name=f'Entering {target_country}',
         data=data,
-        columns=['index', 'frequency'],
+        columns=['country', 'entering_frequency'],
         key_on='feature.properties.name',
-        fill_color=fill_color,
+        fill_color='YlGnBu',
         fill_opacity=0.7,
         line_opacity=0.2,
-        legend_name=title
+        legend_name=f'Entering {target_country}'
     ).add_to(m)
+
+    folium.Choropleth(
+        geo_data=geojson_data,
+        name=f'Leaving {target_country}',
+        data=data,
+        columns=['country', 'leaving_frequency'],
+        key_on='feature.properties.name',  
+        fill_color='YlOrRd',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name=f'Leaving {target_country}'
+    ).add_to(m)
+
+    folium.Choropleth(
+        geo_data=geojson_data,
+        name=f'Net Movement in relation to {target_country}',
+        data=data,
+        columns=['country', 'net_frequency'],
+        key_on='feature.properties.name',
+        fill_color='PiYG',
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name=f'Net Movement in relation to {target_country}'
+    ).add_to(m)
+
     folium.LayerControl().add_to(m)
     return m
 
@@ -64,102 +96,14 @@ def main():
     
     countries = [
         'Portugal', 'United States of America', 'United Kingdom', 'France',
-        'South Africa', 'Italy', 'Japan', 'Denmark', 'Taiwan', 'Russia',
         # Add the remaining countries as needed...
     ]
     
     target_country = st.selectbox('Select a Country', countries)
-    
-    entering_freq, leaving_freq = process_data(df, target_country)
-    
-    if st.button('Show Entering Map'):
-        m_entering = plot_choropleth(geojson_data, entering_freq, f'Entering {target_country}', 'YlGnBu')
-        folium_static(m_entering)
-    
-    if st.button('Show Leaving Map'):
-        m_leaving = plot_choropleth(geojson_data, leaving_freq, f'Leaving {target_country}', 'YlOrRd')
-        folium_static(m_leaving)
+    if target_country:
+        data = process_data(df, target_country)
+        m = plot_choropleth(geojson_data, data, target_country)
+        folium_static(m)
 
 if __name__ == '__main__':
     main()
-
-
-
-# import streamlit as st
-# import folium
-# import pandas as pd
-# from streamlit_folium import folium_static
-# import scipy.sparse
-# from sklearn.metrics.pairwise import cosine_similarity
-
-# @st.cache
-# def load_tfidf():
-#     auths = scipy.sparse.load_npz("transformed_authors.npz")
-#     titles = scipy.sparse.load_npz("transformed_titles.npz")
-#     return auths, titles
-
-
-# def find_function(path,indexed_journeys_df):
-#     row = indexed_journeys_df.loc[path]
-#     return row
-
-# def plot_map(my_row):
-#     start_loc = (my_row['latitude'][0],my_row['longitude'][0])
-#     m = folium.Map(location=start_loc, zoom_start=5)
-#     for i, val in enumerate(my_row['country']):
-#         country = val
-#         location = (my_row['latitude'][i], my_row['longitude'][i])
-#         popup = f"{my_row['title'][i]} {val} {my_row['year'][i]}"
-#         folium.Marker(location=location, popup=popup, icon=folium.Icon(color='blue')).add_to(m)
-#         if i < len(my_row['country'])-1:
-#             location_next = (my_row['latitude'][i+1], my_row['longitude'][i+1])
-#             folium.PolyLine(locations=[location, location_next], color='green', weight=3).add_to(m)
-#     folium_static(m)
-
-# def advanced_find(name, title):
-#     name_vector = auths.transform([name])
-#     ##title_vector = titles.transform([title])
-#     #power_search = st.text_input("Search Papers by Title")
-#     #power_vector = trainer.transform([power_search])
-#     # word_list = power_search.split(" ") 
-#     # word_vector = trainer.transform(word_list) 
-#     # st.write(word_vector)
-#     # find the cosine similarity between the power search and the first 200 papers
-    
-#     cos_sim = cosine_similarity(name_vector, tester)
-#     # find the index of the paper with the highest cosine similarity
-#     matching_index = cos_sim.argmax()
-#     # find the indexis of the top 10 papers with the highest cosine similarity
-#     top_ten = cos_sim.argsort()[0][-10:]
-#     top_ten = top_ten[::-1]
-#     top_sim = cos_sim[0][top_ten]
-#     # find the title of the paper with the highest cosine similarity
-#     matching_data = journeys_df.iloc[matching_index,:]
-    
-#     # change the index of top_ten_titles to the similarity scores
-#     matching_data.index = top_sim
-#     matching_data.index.name = "Similarity Score"
-#     st.write("Top Ten Results")
-#     st.write(matching_data)
-
-# def main():
-#     alt_df = pd.read_hdf('Path_By_Researchers_With_Year.h5')
-#     journeys_df = pd.read_hdf('author_journeys.h5')
-#     journeys_df = alt_df
-#     indexed_journeys_df = journeys_df.set_index('@path', inplace=False)
-
-#     auths, titles = load_tfidf()
-    
-#     st.title('Researcher Migration')
-#     path = '/0000-0003-4998-7259'
-#     path = st.text_input("Write Path Here",path)
-#     on = st.toggle('Advanced Search')
-#     if on:
-#         name = st.text_input("Input researcher name")
-#         title = st.text_input("Input a paper title to assist the search")
-#         advanced_find(name, title)
-#     row = find_function(path,indexed_journeys_df)
-#     plot_map(row)
-
-# if __name__ == "__main__":
-#     main()
